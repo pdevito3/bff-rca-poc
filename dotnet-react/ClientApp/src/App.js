87b72733429598817930c3e31ddd354bbaa85cdb
Paul DeVito <noreply@wrapt.com>
2021-06-08 21:19:56 -0400

    Login/Logout working 🚀

--- dotnet-react/ClientApp/src/App.js
+++ dotnet-react/ClientApp/src/App.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import useUser from './apis/auth/user'
 
 function App() {
+  const claims = useUser();
+  let data = claims?.data;
+  let logoutUrl = data?.find(claim => claim.type === 'bff:logout_url')
+  let name = data?.find(claim => claim.type === 'name') ||  data?.find(claim => claim.type === 'sub');
+  
   return (
-    <div className="text-blue-500 font-bold text-2xl">
-      test
-    </div>
+    <>
+      <header className="bg-indigo-600">
+        <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+          <div className="w-full py-6 flex items-center justify-between">
+            <div className="flex items-center">
+              <a href="#">
+                <span className="sr-only">Workflow</span>
+                <img
+                  className="h-10 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="ml-10 space-x-4">
+              {
+                !name && (
+                  <a
+                    href="/bff/login?returnUrl=/"
+                    className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+                  >
+                    Login
+                  </a>
+                )
+              }
+              {
+                name && (
+                  <a
+                    href={logoutUrl?.value}
+                    className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+                  >
+                    Logout
+                  </a>
+                )
+              }
+              {/* <a
+                href="#"
+                className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
+              >
+                Register
+              </a> */}
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div>
+        {name?.value}
+      </div>
+    </>
   )
 }
 
--- dotnet-react/ClientApp/src/index.js
+++ dotnet-react/ClientApp/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
     <BrowserRouter basename={baseUrl}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
     </BrowserRouter>,
     rootElement);
